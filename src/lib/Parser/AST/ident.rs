4df4d043099986899205734f9d1103079329c2c0
Mitchel Dickerson <mgdicker@uci.edu>
2018-12-19 10:10:10 -0800

    Fixed merging issues still left.

--- src/lib/Parser/AST/ident.rs
+++ src/lib/Parser/AST/ident.rs
@@ -5,22 +5,11 @@ pub struct Ident {
     value: String,
     debugLine: String,
 }
-
-<<<<<<< HEAD
-impl Ident {
-    pub fn new(token: Token) -> Self {
-        //Ident{ token: token,
-        //    value: token.get_contents(),
-        //    debugLine: String::from("test")}
-=======
-//random test
-
 impl Ident {
     pub fn new(token: Token) -> Self {
         Ident{ token: token.clone(),
             value: token.get_contents(),
             debugLine: String::from("test")}
->>>>>>> develop
     }
 
     pub fn get_value(self) -> String {
