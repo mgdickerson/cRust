f07d62c64907df7fba89e839405be50a705001c5
Mitchel Dickerson <mgdicker@uci.edu>
2018-12-19 10:07:19 -0800

    Staging for fix edition.

--- src/lib/Utility/mod.rs
+++ src/lib/Utility/mod.rs
@@ -1,18 +1,2 @@
 use std;
-<<<<<<< HEAD
 use dix;
-=======
-
-pub fn ast_print()
->>>>>>> develop
-
-//|Main
-//  ^
-//  ->  |
-//      |
-//      |
-//
-//  ->  |
-//      |
-//          ^
-//          ->  
